3bd82fc08bbd7c6934aaf1a5309295b9e2c216c7
Leighton Shank <shanklt@jmu.edu>
2015-03-20 02:01:09 -0400

    Works but needs better detectors
    Well it is working, and finding some MDID installations, but there are
    some that slip through the cracks.  Added the file `better_detector.md`
    to track these sites and ideas on how we can improve detecting an MDID
    install, and which version.

---
+++ better_detector.md
@@ -0,0 +1,15 @@
+These sites need better MDID detection
+
+- https://ccdid.coloradocollege.edu/
+  This site is MDID3, but has been customized and the help link has
+  been removed.
+- https://rdidweb.risd.edu/
+  This is an MDID2 install, but it has been customized and the help
+  link id is _ctl1_HelpLink instaed of _ctl0_HelpLink
+- https://vasari.mcah.columbia.edu/
+  A heavily customized version of MDID3
+
+Some ideas for additional MDID3 detectors:
+- `<span id="javascriptwarning">You have JavaScript disabled. MDID works better with JavaScript enabled.</span>`
+- `<div id="basket-content"><div id="basket-scroll-right"></div><div id="basket-scroll-left"></div><div id="basket-thumbs"><br />No image selected.
+</div></div>`
--- mdid.py
+++ mdid.py
@@ -1,14 +1,18 @@
 import requests
 import ssl
+import os.path
+import json
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.poolmanager import PoolManager
 from google import search
 from urlparse import urlparse
 from bs4 import BeautifulSoup
+from blessings import Terminal
 
-hosts_seen = []
-mdid_sites = {}
-result_count = 0
+state_file = 'mdid_stats.json'
+
+requests.packages.urllib3.disable_warnings()
+term = Terminal()
 
 class SSLTLSv1Adapter(HTTPAdapter):
     """Adapter to force SSL TLSv1"""
@@ -24,9 +28,7 @@ def get_hostname(url):
     result = urlparse(url)
     return result.hostname
 
-
-def detect_version(url):
-    """Simple detector to determine what version of MDID a site is running"""
+def make_request(url):
     try:
         r = requests.get(url)
     except requests.exceptions.SSLError:
@@ -39,49 +41,98 @@ def detect_version(url):
         except requests.exceptions.SSLError:
             # if we got an SSLError here, then as a last-ditch we'll try the
             # request again w/o SSL verification
-            try:
-                r = s.get(url, verify=False)
-            except requests.exceptions.SSLError:
-                print "  ERROR!  Too many SSLError exceptions.  Giving up."
-                return None
+            r = s.get(url, verify=False)
 
-    if r.status_code == 200:
+    return r
+
+def detect_version(url):
+    """Simple detector to determine what version of MDID a site is running"""
+    try:
+        r = make_request(url)
+    except:
+        print term.bold_red("error while making request")
+        raise
+
+    version = None
+    server = r.headers.get('server', '')
+    content_type = r.headers['content-type']
+    if r.status_code == 200 and content_type.startswith('text/html'):
         soup = BeautifulSoup(r.content)
         # look for MDID3 by detecting the help link, it seems the most unique
         # <a href="..." id="pagehelp" target="rooibos_help">Help</a>
         a = soup.find(id='pagehelp')
         if a and a.get('target') == 'rooibos_help':
-            return 'MDID3'
+            version = 3
 
         # look for MDID2 by checking the help link also.  why not, eh?
         a = soup.find(id='_ctl0_HelpLink')
         if a and a.get('href') == '/help/helpfile.htm':
-            return 'MDID2'
+            version = 2
 
     # No MDID found
-    return None
+    return (version, server)
 
 # main
-try:
-    for url in search('mdid', only_standard=True, stop=20):
-        # get the hostname from the url
-        hostname = get_hostname(url)
-        # don't process if we've already seen this host
-        if hostname in hosts_seen:
-            continue
+hosts_seen = []
+mdid_sites = {}
+result_count = 0
 
-        print 'examining {url}...'.format(url=url)
-        # try and detect the MDID version running on the host
-        version = detect_version(url)
+# read state
+if os.path.isfile(state_file):
+    f = open(state_file)
+    state = json.loads(f.read())
+    f.close()
+    hosts_seen = state['hosts_seen']
+    mdid_sites = state['mdid_sites']
+    result_count = state['result_count']
+
+stop_limit = result_count + 10
+
+print '-----------------------------------------------------------'
+print 'result count: {count}'.format(count=result_count)
+print '-----------------------------------------------------------'
+
+for url in search('mdid', only_standard=True, start=result_count):
+    hostname = get_hostname(url)
+    # don't process if we've already seen this host
+    if hostname in hosts_seen:
+        continue
+
+    hosts_seen.append(hostname)
+    print 'Checking {url}'.format(url=term.underline_blue(url))
+    # try and detect the MDID version running on the host
+    try:
+        version, server = detect_version(url)
+    except BaseException as e:
+        print term.bold_red('unknown error: {e}').format(e=type(e))
+    else:
         if version:
-            mdid_sites[hostname] = version
-            print '    {host} => {mdid}'.format(host=hostname, mdid=version)
+            mdid_sites[hostname] = {
+                    'version': version,
+                    'server': server,
+                    'url': url}
+            print '  {host} => {version}; {server}'.format(
+                    host=term.green(hostname),
+                    version=term.magenta(str(version)),
+                    server=term.cyan(server))
         else:
-            print '    No MDID installation detected'
-        result_count += 1
-except KeyboardInterrupt:
-    # capture ctrl-c so we can stop the script but still
-    # save our state below
-    # TODO: dump mdid_sites and result_count to json so we can pickup where
-    #       we stopped
-    print 'INTERRUPT!  Saving state and quitting.'
+            print '  No MDID installation detected'
+
+    result_count += 1
+    if result_count >= stop_limit:
+        break
+
+print '-----------------------------------------------------------'
+print 'result count: {count}'.format(count=result_count)
+print '-----------------------------------------------------------'
+
+# save state
+state = {
+    'hosts_seen': hosts_seen,
+    'mdid_sites': mdid_sites,
+    'result_count': result_count
+}
+
+f = open(state_file, 'w')
+f.write(json.dumps(state))
+f.close();
